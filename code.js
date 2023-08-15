9ae1aca59ccc88c2319ed0cf9d002b1b54d39c58
Miken37 <mike-166@hotmail.com>
2023-08-15 11:22:50 -0400

    Working with flex grow to help with boxes fitting inside container

--- code.js
+++ code.js
@@ -3,20 +3,35 @@ function log(text) {
 }
 log(0);
 
+function startGame() {
+    const squaresInput = document.querySelector('.squareCheck');
+    const squaresSubmit = document.querySelector('.squareCheckBtn');
+    squaresSubmit.addEventListener('click', function () {
+        clearGrid();
+        createGrid(squaresInput.value);
+    });
+}
+
 function createGrid(squares) {
     const gridContainer = document.querySelector('.gridContainer');
     let gridSize = squares * squares;
-    const maxSize = 1000;   //Fixed max PIXEL size for grid
+    const maxSize = 900;   //Fixed max PIXEL size for grid
+    let boxSize = (maxSize / squares);  //Calculation to determine size of each box to fit in grid
     for (let i = 1; i <= gridSize; i++) {
         const square = document.createElement('div');
-        let boxSize = (maxSize / squares);  //Calculation to determine size of each box to fit in grid
         square.classList.toggle('square');
-        square.setAttribute('style', `height: ${boxSize}px; width: ${boxSize}px;`)
+        square.id = 'square';
+        square.setAttribute('style', `height: ${boxSize}px; width: ${boxSize}px;`);
         gridContainer.appendChild(square);
-        square.addEventListener('mousemove', function (e) {
-            changeColour(e = square);
+        square.addEventListener('mouseenter', function (e) {
+            changeColour(e.target);
         });
     }
+}
+
+function clearGrid() {
+    const gridAsNode = document.querySelectorAll("#square");
+    gridAsNode.forEach((e) => e.remove());
 }
 
 function changeColour(e) {     //Does stuff when eventlistener is activated in createGrid
@@ -24,5 +39,5 @@ function changeColour(e) {     //Does stuff when eventlistener is activated in c
     e.classList.add('square2');
 }
 
-createGrid(32);
 
+startGame();
